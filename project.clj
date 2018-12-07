7f6974be8bed425fcc81b77798c8a1cca5af89b9
Ed Langley <elangley@cj.com>
2018-12-06 16:15:32 -0800

    rename stuff

--- project.clj
+++ project.clj
@@ -1,9 +1,8 @@
-(defproject lein-autoreload "0.1.1"
+(defproject lein-my-auto-load "0.1.1"
   :description "When running the repl, reload every time a file is saved."
   :url "https://github.com/skilbjo/lein-autoreload"
   :license {:name "MIT"
             :url "http://opensource.org/licenses/MIT"}
-  :dependencies [
-    [leiningen-core "2.7.0"]
-    [ns-tracker "0.2.1"]
-    [org.clojure/clojure "1.8.0"]])
+  :dependencies [[leiningen-core "2.7.0"]
+                 [ns-tracker "0.2.1"]
+                 [org.clojure/clojure "1.8.0"]])
